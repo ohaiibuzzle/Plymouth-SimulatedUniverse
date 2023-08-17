c990d0a59c41def5adbffc3995c5d59c30843c77
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-08-17 14:22:55 +0700

    added: 4k variant

--- install.sh
+++ install.sh
@@ -11,7 +11,24 @@ if [ -d "/usr/share/plymouth/themes/SimulatedUniverse" ]; then
     echo "Removing old theme..."
     rm -rf /usr/share/plymouth/themes/SimulatedUniverse
 fi
-cp -r ./SimulatedUniverse /usr/share/plymouth/themes/
+
+# Ask the user to select the 1080p or 4K variant
+echo "Which variant would you like to install?"
+echo "1. 1080p"
+echo "2. 4K"
+
+read -p "> " variant
+
+if [ "$variant" == "1" ]; then
+    echo "Installing 1080p variant..."
+    cp -r SimulatedUniverse /usr/share/plymouth/themes/SimulatedUniverse
+elif [ "$variant" == "2" ]; then
+    echo "Installing 4K variant..."
+    cp -r SimulatedUniverse-4k /usr/share/plymouth/themes/SimulatedUniverse
+else
+    echo "Invalid option. Please try again."
+    exit
+fi
 
 # Prompt for theme selection
 echo 'Please run "sudo plymouth-set-default-theme SimulatedUniverse" to set the theme.'
